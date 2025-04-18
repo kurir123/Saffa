365673ef70837d88e3ea1693960a70b495d7357c
kurir123 <usfahstore@gmail.com>
2025-04-18 21:05:00 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Kurir App Siap Deploy!</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
